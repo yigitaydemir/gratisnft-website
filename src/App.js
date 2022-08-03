867844548453cc8fad8b197477f91be3b00cea76
Yusuf Yigit Aydemir <yusufyigitaydemir@gmail.com>
2022-08-03 15:52:23 +0300

    taslak hazir

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='content' id='content-1'>
+        <div className='navbar'>
+          <h1>Gratis NFT</h1>
+
+          <ul>
+            <li><a href="#top">Test</a></li>
+            <li><a href="#top">Test</a></li>
+            <li><a href="#top">Test</a></li>
+          </ul>
+        </div>
+
+        <div className='welcome-text'>
+          <h1>Welcome to the Gratis NFT Website</h1>
+          <p>"Greatest of All Bots"</p>
+        </div>
+      </div>
+      
+      <div className='content' id='content-2'>
+        <h1>Content Setion 2</h1>
+      </div>
+
+      <div className='content' id='content-3'>
+        <h1>Content Section 3</h1>
+      </div>
+
+      <div className='content' id='content-4'>
+        <h1>Content Section 4</h1>
+      </div>
+
+      <div className='content' id='content-5'>
+        <h1>Content Section 5</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
